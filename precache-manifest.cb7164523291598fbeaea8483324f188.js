37784fb70c3acf9737fbb9f8063cef2dac86817c
ruok27 <jakxii27@gmail.com>
2020-03-02 02:18:55 -0500

    Updates

---
+++ precache-manifest.cb7164523291598fbeaea8483324f188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b432fe052377b1adb11cb8b2bb84c5bd",
+    "url": "/CanvasJS-demo/index.html"
+  },
+  {
+    "revision": "95ef82f7d2ba100849be",
+    "url": "/CanvasJS-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2d161a57912a9a3294cf",
+    "url": "/CanvasJS-demo/static/js/2.a8262049.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/CanvasJS-demo/static/js/2.a8262049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95ef82f7d2ba100849be",
+    "url": "/CanvasJS-demo/static/js/main.6878d014.chunk.js"
+  },
+  {
+    "revision": "f7e56cb4cf2de32498e9",
+    "url": "/CanvasJS-demo/static/js/runtime-main.55e46a0b.js"
+  }
+]);
